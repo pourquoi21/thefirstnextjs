0e7edb468178a4249c7351afec75d3acb5701b78
pourquoi21 <miyaku219@naver.com>
2023-06-02 15:12:51 +0900

    getting md files as data

---
+++ lib/post.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getSortedPostsData() {
+  //  posts 파일 이름을 잡아준다
+  const fileNames = fs.readdirSync(postsDirectory);
+  //['pre-rendering.md', ...]
+
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, "");
+
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf-8");
+
+    const matterResult = matter(fileContents);
+
+    return {
+      id,
+      ...allPostsData(matterResult.data as { date: string; title: string }),
+    };
+  });
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
